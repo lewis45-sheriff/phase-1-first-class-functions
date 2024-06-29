2109c90599a8432e966ed03be37e863a0b7094b3
lewis <lewiskipkemoi53@gmail.com>
2024-06-29 10:20:55 +0300

    all

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-// Function that receives a function and calls it
-f receivesAFunction(callback) {unction
-    console.log('receivesAFunction called');
-    callback();
+
+function receivesAFunction(callback) {
+  console.log('receivesAFunction called');
+  callback();
+}
+
+function returnsANamedFunction() {
+  console.log('returnsANamedFunction called');
+  function namedFunction() {
+    return 'I am a named function';
   }
-  
-  function returnsANamedFunction() {
-    console.log('returnsANamedFunction called');
-    function namedFunction() {
-      return 'I am a named function';
-    }
-    return namedFunction;
-  }
-  
-  function returnsAnAnonymousFunction() {
-    console.log('returnsAnAnonymousFunction called');
-    return function() {
-      return 'I am an anonymous function';
-    };
-  }
-  
-  
-  
+  return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+  console.log('returnsAnAnonymousFunction called');
+  return function() {
+    return 'I am an anonymous function';
+  };
+}
